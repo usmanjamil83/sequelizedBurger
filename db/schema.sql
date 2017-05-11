9ca11e5f59a3af981a75ee7388afda91158523cf
usmanjamil83 <usmanjamil83@hotmail.com>
2017-05-11 16:13:39 -0400

    worked on every thing

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+### Schema
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	Burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (Burger_name)
+VALUES ('Buzz Burgers');
+INSERT INTO burgers (Burger_name)
+VALUES ('Buffalo Burgers');
+INSERT INTO burgers (Burger_name)
+VALUES ('Backyard Burgers');
